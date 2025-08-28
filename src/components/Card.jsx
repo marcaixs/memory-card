b6d5560001d5d373212dc66b6ffe361651cddf3f
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-28 16:57:45 +0200

    add logic for increment current points and game over

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,18 @@
-export default function Card({pokemon, selectedPokemons, setSelectedPokemons}){
+export default function Card({pokemon, selectedPokemons, setSelectedPokemons, currentScore, setCurrentScore, gameOver, setGameOver}){
     
     return(
-        <div className="card" key={pokemon.id} onClick={()=> setSelectedPokemons([...selectedPokemons, pokemon.id])}>
+        <div className="card" key={pokemon.id} onClick={()=>{
+            const found = selectedPokemons.find((element)=> element == pokemon.id)
+            if(!found){
+                setSelectedPokemons([...selectedPokemons, pokemon.id]);
+                setCurrentScore(currentScore + 1);
+            } else {
+                alert('Game Over!')
+                setCurrentScore(0);
+                setGameOver(!gameOver);
+            }
+           
+        }}>
             <img src={pokemon.sprites.front_default} alt={pokemon.name} />
             <p>{pokemon.name}</p>
         </div>

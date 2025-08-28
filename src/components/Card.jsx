1a6f6eb56e240470d397a273b080bf801d0f1229
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-28 19:06:23 +0200

    add winning condition

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,9 +5,15 @@ export default function Card({pokemon, selectedPokemons, setSelectedPokemons, cu
             if(!found){ //if not, add to the selected pokemons array
                 setSelectedPokemons([...selectedPokemons, pokemon.id]);
                 setCurrentScore(currentScore + 1); //and increments score
+                if(currentScore == 15) { //if score reach maximum pokemons
+                    setSelectedPokemons([]); //clean array of selected pokemons
+                    alert('You Win!') //display game over alert
+                    setGameOver(!gameOver) //update game over state
+                }
             } else { //if the pokemon has already been selected
                 alert('Game Over!') //display game over alert
                 setCurrentScore(0); //set current score to 0
+                setSelectedPokemons([]); //clean array of selected pokemons
                 setGameOver(!gameOver); //update the gameOver state
             }
     }

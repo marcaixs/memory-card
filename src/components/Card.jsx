18e9afac81a486a8ee0dec342c32e43db3415558
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-28 12:29:31 +0200

    render name and sprite of pokemon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,9 @@
-export default function Card(){
+export default function Card({pokemon}){
+    console.log(pokemon)
     return(
-    <div>
-
+    <div className="card" key={pokemon.id}>
+        <img src={pokemon.sprites.front_default} alt="" />
+        <p>{pokemon.name}</p>
     </div>
     )
 }

9329630c142ffcbc85537b080a382d9f261c2880
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-28 15:44:37 +0200

    make img alt to be the name of pokemon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ export default function Card({pokemon}){
     console.log(pokemon)
     return(
     <div className="card" key={pokemon.id}>
-        <img src={pokemon.sprites.front_default} alt="" />
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
         <p>{pokemon.name}</p>
     </div>
     )

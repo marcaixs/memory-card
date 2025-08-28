facb22047cd48261f6c68279d52045209c88f748
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-28 12:30:07 +0200

    fetch pokeapi and pas json to card prop

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,9 +1,24 @@
-
+import Card from "./Card"
+import { useState, useEffect } from 'react'
 
 export default function CardsContainer(){
-    return(
-    <div>
+    const [pokemons, setPokemons] = useState([]);
 
-    </div>
+    useEffect(() => {
+        
+        fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+        .then(response => response.json())
+        .then(json => setPokemons([...pokemons, json]))
+        .catch(error => console.error(error));
+        
+    }, []);
+
+
+    return(
+        <div className="card-container">
+            {pokemons.map((pokemon)=>
+                <Card pokemon={pokemon}/>
+            )}
+        </div>
     )
 }

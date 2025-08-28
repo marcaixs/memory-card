e2bdc7e0542f48ac205090507833f63e3f792e70
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-28 16:26:21 +0200

    pass selectedPokemon state and set from props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
-export default function Card({pokemon}){
-    console.log(pokemon)
+export default function Card({pokemon, selectedPokemons, setSelectedPokemons}){
+    
     return(
-    <div className="card" key={pokemon.id}>
-        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-        <p>{pokemon.name}</p>
-    </div>
+        <div className="card" key={pokemon.id} onClick={()=> setSelectedPokemons([...selectedPokemons, pokemon.id])}>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <p>{pokemon.name}</p>
+        </div>
     )
 }

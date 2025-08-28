c6247c39d2379bc0640f646735f83f6cb5d20432
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-28 15:43:55 +0200

    call ten random pokemon and store into the state

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -5,16 +5,37 @@ export default function CardsContainer(){
     const [pokemons, setPokemons] = useState([]);
 
     useEffect(() => {
-        
-        fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-        .then(response => response.json())
-        .then(json => setPokemons([...pokemons, json]))
-        .catch(error => console.error(error));
-        
+        const fetchPokemons = async () => {
+            try {
+                const promises = []; //store pokemons from the api
+                const randomIds = []; //store random ids 
+
+                //create random ids and push into the array only if its unique
+                while (randomIds.length < 10){
+                    const randomId = Math.floor(Math.random() * 151) + 1;
+                    if(randomIds.indexOf(randomId) === -1) randomIds.push(randomId);
+                }
+                
+                //make a call to the api for each random id stored in the array
+                randomIds.map(randomId => promises.push(
+                    fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`).then(res => res.json())
+                ));
+                
+                // await to end all fetches
+                const results = await Promise.all(promises);
+
+                // store in the state
+                setPokemons(results);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        //call the async function
+        fetchPokemons();
+           
     }, []);
 
-
-    return(
+    return(   
         <div className="card-container">
             {pokemons.map((pokemon)=>
                 <Card pokemon={pokemon}/>

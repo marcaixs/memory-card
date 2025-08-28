721b8b916e56061c86d84773953bd0cb7430a575
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-28 17:20:41 +0200

    add onclick logic to a separare function and comments

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,20 @@
 export default function Card({pokemon, selectedPokemons, setSelectedPokemons, currentScore, setCurrentScore, gameOver, setGameOver}){
-    
-    return(
-        <div className="card" key={pokemon.id} onClick={()=>{
-            const found = selectedPokemons.find((element)=> element == pokemon.id)
-            if(!found){
+    //function that executes on onClick card listener
+    function cardClick(pokemon, selectedPokemons, setSelectedPokemons, currentScore, setCurrentScore, gameOver, setGameOver){
+        const found = selectedPokemons.find((element)=> element == pokemon.id) //check if the pokemon is already selected
+            if(!found){ //if not, add to the selected pokemons array
                 setSelectedPokemons([...selectedPokemons, pokemon.id]);
-                setCurrentScore(currentScore + 1);
-            } else {
-                alert('Game Over!')
-                setCurrentScore(0);
-                setGameOver(!gameOver);
+                setCurrentScore(currentScore + 1); //and increments score
+            } else { //if the pokemon has already been selected
+                alert('Game Over!') //display game over alert
+                setCurrentScore(0); //set current score to 0
+                setGameOver(!gameOver); //update the gameOver state
             }
-           
-        }}>
+    }
+
+    return(
+        <div className="card" key={pokemon.id} onClick={()=>
+            cardClick(pokemon, selectedPokemons, setSelectedPokemons, currentScore, setCurrentScore, gameOver, setGameOver)}>
             <img src={pokemon.sprites.front_default} alt={pokemon.name} />
             <p>{pokemon.name}</p>
         </div>
